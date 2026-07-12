9d5f1914c715cb20dbe6e9ec90dcb5f6ffe06556
uronemi <uronemi@gmail.com>
2026-07-12 23:36:17 +0900

    .

--- datastructure/LazySegtree.hpp
+++ datastructure/LazySegtree.hpp
@@ -1,38 +1,36 @@
 #pragma once
+#include "../utility/Algebra.hpp"
 
 // Lazy Segment Tree
 //
 // モノイドを載せると区間更新・区間集約ができる
-template <
-	typename S,
-	auto op,
-	auto e,
-	typename F,
-	auto mapping,
-	auto composition,
-	auto id
->
+template <IsActedMonoid M>
 class LazySegtree {
-	private:
-	std::vector<S> dat;
-	std::vector<F> lazy;
-	unsigned int N, sz, log;
+	public:
+	using value_type = M::value_type;
+	using operator_type = M::operator_type;
+	using idx_type = unsigned int;
 
-	void propagate(unsigned int idx, const F &f) {
-		dat[idx] = mapping(f, dat[idx]);
-		if (idx < sz) lazy[idx] = composition(f, lazy[idx]);
+	private:
+	std::vector<value_type> dat;
+	std::vector<operator_type> lazy;
+	idx_type N, sz, log;
+
+	void propagate(idx_type idx, const operator_type &f) {
+		dat[idx] = M::mapping(f, dat[idx]);
+		if (idx < sz) lazy[idx] = M::composition(f, lazy[idx]);
 	}
 
-	void pushdown(unsigned int idx) {
-		if (lazy[idx] == id()) return;
+	void pushdown(idx_type idx) {
+		if (lazy[idx] == M::id()) return;
 		propagate(idx * 2, lazy[idx]);
 		propagate(idx * 2 + 1, lazy[idx]);
-		lazy[idx] = id();
+		lazy[idx] = M::id();
 	}
 
-	void merge(unsigned int idx) { dat[idx] = op(dat[idx * 2], dat[idx * 2 + 1]); }
+	void merge(idx_type idx) { dat[idx] = M::op(dat[idx * 2], dat[idx * 2 + 1]); }
 
-	void pushup(unsigned int idx) {
+	void pushup(idx_type idx) {
 		while (idx > 1) {
 			idx >>= 1;
 			merge(idx);
@@ -42,151 +40,165 @@ class LazySegtree {
 	public:
 	LazySegtree() {}
 
-	explicit LazySegtree(unsigned int N_) : LazySegtree(std::vector<S>(N_, e())) {}
+	explicit LazySegtree(idx_type N_) : LazySegtree(std::vector<value_type>(N_, M::e())) {}
 
-	LazySegtree(unsigned int N_, const S &x) : LazySegtree(std::vector<S>(N_, x)) {}
+	LazySegtree(idx_type N_, const value_type &x) : LazySegtree(std::vector<value_type>(N_, x)) {}
 
 	// v から Segtree を構築
 	// O(|v|) time
-	explicit LazySegtree(const std::vector<S>& v) : N(v.size()) {
+	explicit LazySegtree(const std::vector<value_type>& v) : N(v.size()) {
 		sz = std::bit_ceil(N);
 		log = std::bit_width(sz - 1);
-		dat.assign(sz * 2, e());
-		lazy.assign(sz * 2, id());
-		for (unsigned int i = 0; i < N; ++i) dat[i + sz] = v[i];
-		for (unsigned int i = sz - 1; i >= 1; --i) dat[i] = op(dat[i * 2], dat[i * 2 + 1]);
+		dat.assign(sz * 2, M::e());
+		lazy.assign(sz * 2, M::id());
+		for (idx_type i = 0; i < N; ++i) dat[i + sz] = v[i];
+		for (idx_type i = sz - 1; i >= 1; --i) merge(i);
 	}
 
 	// 指定した位置の値を変更
+	//
 	// - idx < N
+	//
 	// O(logN) time
-	void set(unsigned int idx, const S &x) {
+	void set(idx_type idx, const value_type &x) {
 		assert(idx < N);
 		idx += sz;
-		for (unsigned int i = log; i >= 1; --i) pushdown(idx >> i);
+		for (idx_type i = log; i >= 1; --i) pushdown(idx >> i);
 		dat[idx] = x;
 		pushup(idx);
 	}
 
 	// 指定した位置に値を加算
+	//
 	// - idx < N
 	// - S に operator+= が定義されている
+	//
 	// O(logN) time
-	void add(unsigned int idx, const S &x) {
+	void add(idx_type idx, const value_type &x) {
 		assert(idx < N);
 		idx += sz;
-		for (unsigned int i = log; i >= 1; --i) pushdown(idx >> i);
+		for (idx_type i = log; i >= 1; --i) pushdown(idx >> i);
 		dat[idx] += x;
 		pushup(idx);
 	}
 
 	// 指定した位置に f を作用させる
+	//
 	// - idx < N
+	//
 	// O(logN) time
-	void apply(int idx, const F &f) {
+	void apply(int idx, const operator_type &f) {
 		assert(idx < N);
 		idx += sz;
-		for (unsigned int i = log; i >= 1; --i) pushdown(idx >> i);
-		dat[idx] = mapping(f, dat[idx]);
+		for (idx_type i = log; i >= 1; --i) pushdown(idx >> i);
+		dat[idx] = M::mapping(f, dat[idx]);
 		pushup(idx);
 	}
 
 	// 区間 [l, r) に f を作用させる
+	//
 	// - l <= r && r <= N
+	//
 	// O(logN) time
-	void apply(unsigned int l, unsigned int r, const F &f) {
+	void apply(idx_type l, idx_type r, const operator_type &f) {
 		assert(l <= r && r <= N);
 		if (l == r) return;
 		l += sz, r += sz;
-		for (unsigned int i = log; i >= 1; --i) {
+		for (idx_type i = log; i >= 1; --i) {
 			if (((l >> i) << i) != l) pushdown(l >> i);
 			if (((r >> i) << i) != r) pushdown((r - 1) >> i);
 		}
-		for (unsigned int l2 = l, r2 = r; l2 < r2; l2 >>= 1, r2 >>= 1) {
+		for (idx_type l2 = l, r2 = r; l2 < r2; l2 >>= 1, r2 >>= 1) {
 			if (l2 & 1) propagate(l2++, f);
 			if (r2 & 1) propagate(--r2, f);
 		}
-		for (unsigned int i = 1; i <= log; ++i) {
+		for (idx_type i = 1; i <= log; ++i) {
 			if (((l >> i) << i) != l) merge(l >> i);
 			if (((r >> i) << i) != r) merge((r - 1) >> i);
 		}
 	}
 
 	// 指定した位置の値を返す
+	//
 	// - idx < N
+	//
 	// O(logN) time
-	S get(unsigned int idx) {
+	value_type get(idx_type idx) {
 		assert(idx < N);
 		idx += sz;
-		for (unsigned int i = log; i >= 1; --i) pushdown(idx >> i);
+		for (idx_type i = log; i >= 1; --i) pushdown(idx >> i);
 		return dat[idx];
 	}
 
 	// 指定した位置の値を返す（境界チェックなし）
-	S operator[](unsigned int idx) noexcept {
+	value_type operator[](idx_type idx) noexcept {
 		idx += sz;
-		for (unsigned int i = log; i >= 1; --i) pushdown(idx >> i);
+		for (idx_type i = log; i >= 1; --i) pushdown(idx >> i);
 		return dat[idx];
 	}
 
 	// 区間 [l, r) の値の総積を返す
+	//
 	// - l <= r && r <= N
+	//
 	// O(logN) time
-	S fold(unsigned int l, unsigned int r) {
+	value_type fold(idx_type l, idx_type r) {
 		assert(l <= r && r <= N);
-		if (l == r) return e();
+		if (l == r) return M::e();
 		l += sz, r += sz;
-		for (unsigned int i = log; i >= 1; --i) {
+		for (idx_type i = log; i >= 1; --i) {
 			if (((l >> i) << i) != l) pushdown(l >> i);
 			if (((r >> i) << i) != r) pushdown((r - 1) >> i);
 		}
-		S resl = e(), resr = e();
+		value_type resl = M::e(), resr = M::e();
 		for (; l < r; l >>= 1, r >>= 1) {
-			if (l & 1) resl = op(resl, dat[l++]);
-			if (r & 1) resr = op(dat[--r], resr);
+			if (l & 1) resl = M::op(resl, dat[l++]);
+			if (r & 1) resr = M::op(dat[--r], resr);
 		}
-		return op(resl, resr);
+		return M::op(resl, resr);
 	}
 
 	// 列全体の値の総積を返す
 	// O(1) time
-	S all_fold() const { return dat[1]; }
+	value_type all_fold() const { return dat[1]; }
 
 	template <auto g>
-	unsigned int max_right(unsigned int l) {
-		return max_right(l, [](const S &x) -> bool { return g(x); });
+	idx_type max_right(idx_type l) {
+		return max_right(l, [](const value_type& x) -> bool { return g(x); });
 	}
 
 	// f が単調のとき g(fold(l, r)) = true になる最大の r を返す
+	//
 	// - l <= N
 	// - g(e()) = true
+	//
 	// O(logN) time
 	template <typename G>
-	unsigned int max_right(unsigned int l, const G &g) {
+	idx_type max_right(idx_type l, const G& g) {
 		assert(l <= N);
-		assert(g(e()));
+		assert(g(M::e()));
 		if (l == N) return N;
 		l += sz;
-		for (unsigned int i = log; i >= 1; --i) pushdown(l >> i);
-		S prod = e();
+		for (idx_type i = log; i >= 1; --i) pushdown(l >> i);
+		value_type prod = M::e();
 		do {
 			while (!(l & 1)) l >>= 1;
-			if (!g(op(prod, dat[l]))) {
+			if (!g(M::op(prod, dat[l]))) {
 				while (l < sz) {
 					pushdown(l);
 					l <<= 1;
-					if (g(op(prod, dat[l]))) prod = op(prod, dat[l++]);
+					if (g(M::op(prod, dat[l]))) prod = M::op(prod, dat[l++]);
 				}
 				return l - sz;
 			}
-			prod = op(prod, dat[l++]);
+			prod = M::op(prod, dat[l++]);
 		} while ((l & -l) != l);
 		return N;
 	}
 
 	template <auto g>
-	unsigned int min_left(unsigned int r) {
-		return min_left(r, [](const S &x) -> bool { return g(x); });
+	idx_type min_left(idx_type r) {
+		return min_left(r, [](const value_type& x) -> bool { return g(x); });
 	}
 
 	// f が単調のとき g(fold(l, r)) = true になる最小の l を返す
@@ -194,25 +206,25 @@ class LazySegtree {
 	// - g(e()) = true
 	// O(logN) time
 	template <typename G>
-	unsigned int min_left(unsigned int r, const G& g) {
+	idx_type min_left(idx_type r, const G& g) {
 		assert(r <= N);
-		assert(g(e()));
+		assert(g(M::e()));
 		if (r == 0) return 0;
 		r += sz;
-		for (unsigned int i = log; i >= 1; --i) pushdown((r - 1) >> i);
-		S prod = e();
+		for (idx_type i = log; i >= 1; --i) pushdown((r - 1) >> i);
+		value_type prod = M::e();
 		do {
 			--r;
 			while (r > 1 && (r & 1)) r >>= 1;
-			if (!g(op(dat[r], prod))) {
+			if (!g(M::op(dat[r], prod))) {
 				while (r < sz) {
 					pushdown(r);
 					r = r * 2 + 1;
-					if (g(op(dat[r], prod))) prod = op(dat[r--], prod);
+					if (g(op(dat[r], prod))) prod = M::op(dat[r--], prod);
 				}
 				return r + 1 - sz;
 			}
-			prod = op(dat[r], prod);
+			prod = M::op(dat[r], prod);
 		} while ((r & -r) != r);
 		return 0;
 	}
--- datastructure/Segtree.hpp
+++ datastructure/Segtree.hpp
@@ -1,42 +1,44 @@
 #pragma once
-#include "../utility/Template.hpp"
+#include "../utility/Algebra.hpp"
 
 // Segment Tree
 //
 // モノイドを載せると一点更新・区間集約ができる
-template <
-  typename S,
-  auto op,
-  auto e
->
+template <IsMonoid M>
 class Segtree {
-  private:
-  unsigned int N, sz;
-  std::vector<S> dat;
+  public:
+  using value_type = M::value_type;
+  using idx_type = unsigned int;
 
-  void pushup(unsigned int idx) { dat[idx] = op(dat[idx * 2], dat[idx * 2 + 1]); }
+  private:
+  idx_type N, sz;
+  std::vector<value_type> dat;
+
+  void pushup(idx_type idx) { dat[idx] = M::op(dat[idx * 2], dat[idx * 2 + 1]); }
 
   public:
   Segtree() {}
 
-  explicit Segtree(unsigned int N_) : Segtree(std::vector<S>(N_, e())) {}
+  explicit Segtree(idx_type N_) : Segtree(std::vector<value_type>(N_, M::e())) {}
 
-  explicit Segtree(unsigned int N_, const S &x) : Segtree(std::vector<S>(N_, x)) {}
+  explicit Segtree(idx_type N_, const value_type &x) : Segtree(std::vector<value_type>(N_, x)) {}
 
   // v から Segtree を構築
   // O(|v|) time
-  explicit Segtree(const std::vector<S> &v) : N(v.size()) {
+  explicit Segtree(const std::vector<value_type> &v) : N(v.size()) {
     sz = std::bit_ceil(N);
-    dat.assign(sz * 2, e());
+    dat.assign(sz * 2, M::e());
 
-    for (unsigned int i = 0; i < N; ++i) dat[sz + i] = v[i];
-    for (unsigned int i = sz - 1; i >= 1; --i) pushup(i);
+    for (idx_type i = 0; i < N; ++i) dat[sz + i] = v[i];
+    for (idx_type i = sz - 1; i >= 1; --i) pushup(i);
   }
 
   // 指定した位置の値を変更
+  //
   // - idx < N
+  //
   // O(logN) time
-  void set(unsigned int idx, const S &x) {
+  void set(idx_type idx, const value_type &x) {
     assert(idx < N);
     idx += sz;
     dat[idx] = x;
@@ -47,10 +49,12 @@ class Segtree {
   }
 
   // 指定した位置に値を加算
+  //
   // - S に operator+= が定義されている
   // - idx < N
+  //
   // O(logN) time
-  void add(unsigned int idx, const S &x) {
+  void add(idx_type idx, const value_type &x) {
     assert(idx < N);
     idx += sz;
     dat[idx] += x;
@@ -61,84 +65,90 @@ class Segtree {
   }
 
   // 区間 [l, r) の値の総積を返す
+  //
   // - l <= r <= N
+  //
   // O(logN) time
-  S fold(unsigned int l, unsigned int r) const {
+  value_type fold(idx_type l, idx_type r) const {
     assert(l <= r && r <= N);
-    S resl = e(), resr = e();
+    value_type resl = M::e(), resr = M::e();
 
     for (l += sz, r += sz; l < r; l >>= 1, r >>= 1) {
-      if (l & 1) resl = op(resl, dat[l++]);
-      if (r & 1) resr = op(dat[--r], resr);
+      if (l & 1) resl = M::op(resl, dat[l++]);
+      if (r & 1) resr = M::op(dat[--r], resr);
     }
 
-    return op(resl, resr);
+    return M::op(resl, resr);
   }
 
   // 列全体の値の総積を返す
   // O(1) time
-  S all_fold() const noexcept { return dat[1]; }
+  value_type all_fold() const noexcept { return dat[1]; }
 
   // 指定した位置の値を返す
   // O(1) time
-  S get(unsigned int idx) const {
+  value_type get(idx_type idx) const {
     assert(idx < N);
     return dat[idx + sz];
   }
 
   // 指定した位置の値を返す（境界チェックなし）
-  S operator[](unsigned int idx) const noexcept { return dat[idx + sz]; }
+  value_type operator[](idx_type idx) const noexcept { return dat[idx + sz]; }
 
   // f が単調なとき，f(fold(l, r)) が true になる最大の r を返す
+  //
   // - l <= N
   // - f(e()) = true
+  //
   // O(logN) time
   template<typename F>
-  unsigned int max_right(unsigned int l, const F &f) const {
+  idx_type max_right(idx_type l, const F &f) const {
     assert(l <= N);
-    assert(f(e()));
+    assert(f(M::e()));
     if (l == N) return N;
     l += sz;
-    S prod = e();
+    value_type prod = M::e();
 
     do {
       while (!(l & 1)) l >>= 1;
-      if (!f(op(prod, dat[l]))) {
+      if (!f(M::op(prod, dat[l]))) {
         while (l < sz) {
           l <<= 1;
-          if (f(op(prod, dat[l]))) prod = op(prod, dat[l++]);
+          if (f(M::op(prod, dat[l]))) prod = M::op(prod, dat[l++]);
         }
         return l - sz;
       }
-      prod = op(prod, dat[l++]);
+      prod = M::op(prod, dat[l++]);
     } while ((l & -l) != l);
     
     return N;
   }
   
   // f が単調なとき，f(fold(l, r)) が true になる最小の l を返す
+  //
   // - r <= N
   // - f(e()) = true
+  //
   // O(logN) time
   template <typename F>
-  unsigned int min_left(unsigned int r, const F &f) const {
+  idx_type min_left(idx_type r, const F &f) const {
     assert(r <= N);
-    assert(f(e()));
+    assert(f(M::e()));
     if (r == 0) return 0;
     r += sz;
-    S prod = e();
+    value_type prod = M::e();
 
     do {
       --r;
       while (r > 1 && (r & 1)) r >>= 1;
-      if (!f(op(dat[r], prod))) {
+      if (!f(M::op(dat[r], prod))) {
         while (r < sz) {
           r = r * 2 + 1;
-          if (f(op(dat[r], prod))) prod = op(dat[r--], prod);
+          if (f(M::op(dat[r], prod))) prod = M::op(dat[r--], prod);
         }
         return r + 1 - sz;
       }
-      prod = op(dat[r], prod);
+      prod = M::op(dat[r], prod);
     } while ((r & -r) != r);
 
     return 0;
